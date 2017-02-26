8fd59eaa9909e6da9fcdcbafd925beeb57e3a9db
Jean-François Beauchef <jean-francois@beauchef.com>
2017-02-26 16:27:31 -0500

    Lab 6

---
+++ compute.tf
@@ -0,0 +1,137 @@
+resource "google_compute_instance" "jumphost" {
+    name = "jumphost"
+    machine_type = "f1-micro"
+    zone = "us-east1-b"
+    tags = ["public-node"]
+
+    disk {
+        image = "debian-cloud/debian-8"
+    }
+
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.public.name}"
+        access_config {}
+    }
+
+    metadata_startup_script = "apt-get -y install apache2 && systemctl start apache2"
+}
+
+resource "google_compute_instance" "vaulthost" {
+    name = "vaulthost"
+    machine_type = "f1-micro"
+    zone = "us-east1-b"
+    tags = ["public-node"]
+
+    disk {
+        image = "coreos-cloud/coreos-stable"
+    }
+
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.public.name}"
+        access_config {}
+    }
+
+    metadata_startup_script = "apt-get -y install apache2 && systemctl start apache2"
+}
+
+resource "google_compute_instance" "master" {
+    name = "master"
+    machine_type = "f1-micro"
+    zone = "us-east1-b"
+    tags = ["workload-node"]
+
+    disk {
+        image = "coreos-cloud/coreos-stable"
+    }
+
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.workload.name}"
+    }
+
+    metadata_startup_script = "apt-get -y install apache2 && systemctl start apache2"
+}
+
+resource "google_compute_instance_template" "worker" {
+  name                 = "worker"
+  machine_type         = "f1-micro"
+  can_ip_forward       = false
+  tags = ["workload-node"]
+
+  disk {
+    source_image = "coreos-cloud/coreos-stable"
+    auto_delete = true
+    boot = true
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.workload.name}"
+  }
+}
+
+resource "google_compute_instance_group_manager" "workers" {
+  name               = "workers"
+  base_instance_name = "worker"
+  instance_template  = "${google_compute_instance_template.worker.self_link}"
+  zone               = "us-east1-b"
+}
+
+resource "google_compute_autoscaler" "workscaler" {
+  name   = "workscaler"
+  zone   = "us-east1-b"
+  target = "${google_compute_instance_group_manager.workers.self_link}"
+
+  autoscaling_policy = {
+    max_replicas    = 5
+    min_replicas    = 2
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.5
+    }
+  }
+}
+
+resource "google_compute_instance" "etcd1" {
+    name                 = "etcd1"
+    machine_type         = "f1-micro"
+    zone                 = "us-east1-b"
+    tags                 = ["backend-node"]
+
+    disk {
+        image = "coreos-cloud/coreos-stable"
+    }
+
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.backend.name}"
+    }
+}
+
+resource "google_compute_instance" "etcd2" {
+    name                 = "etcd2"
+    machine_type         = "f1-micro"
+    zone                 = "us-east1-b"
+    tags                 = ["backend-node"]
+
+    disk {
+        image = "coreos-cloud/coreos-stable"
+    }
+
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.backend.name}"
+    }
+}
+
+resource "google_compute_instance" "etcd3" {
+    name                 = "etcd3"
+    machine_type         = "f1-micro"
+    zone                 = "us-east1-b"
+    tags                 = ["backend-node"]
+
+    disk {
+        image = "coreos-cloud/coreos-stable"
+    }
+
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.backend.name}"
+    }
+}
---
+++ networking.tf
@@ -0,0 +1,89 @@
+resource "google_compute_network" "cr460" {
+    name                    = "cr460"
+    auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "public" {
+    name          = "public"
+    ip_cidr_range = "172.16.1.0/24"
+    network       = "${google_compute_network.cr460.self_link}"
+    region        = "us-east1"
+}
+
+resource "google_compute_subnetwork" "workload" {
+    name          = "workload"
+    ip_cidr_range = "10.0.1.0/24"
+    network       = "${google_compute_network.cr460.self_link}"
+    region        = "us-east1"
+}
+
+resource "google_compute_subnetwork" "backend" {
+    name          = "backend"
+    ip_cidr_range = "10.0.2.0/24"
+    network       = "${google_compute_network.cr460.self_link}"
+    region        = "us-east1"
+}
+
+resource "google_compute_firewall" "public-firewall" {
+    name = "public-firewall"
+    network = "${google_compute_network.cr460.name}"
+    allow {
+        protocol = "tcp"
+        ports = ["22", "80", "443"]
+    }
+    target_tags = ["public-node"]
+}
+
+resource "google_compute_firewall" "public-workload-firewall" {
+    name = "public-workload-firewall"
+    network = "${google_compute_network.cr460.name}"
+    allow {
+        protocol = "tcp"
+        ports = ["22"]
+    }
+    source_tags = ["public-node"]
+    target_tags = ["workload-node"]
+}
+
+resource "google_compute_firewall" "public-backend-firewall" {
+    name = "public-backend-firewall"
+    network = "${google_compute_network.cr460.name}"
+    allow {
+        protocol = "tcp"
+        ports = ["22", "2379", "2380"]
+    }
+    source_tags = ["public-node"]
+    target_tags = ["backend-node"]
+}
+
+resource "google_compute_firewall" "workload-backend-firewall" {
+    name = "workload-backend-firewall"
+    network = "${google_compute_network.cr460.name}"
+    allow {
+        protocol = "tcp"
+        ports = ["22", "2379", "2380"]
+    }
+    source_tags = ["workload-node"]
+    target_tags = ["backend-node"]
+}
+
+resource "google_dns_managed_zone" "beauchef" {
+  name     = "beauchef"
+  dns_name = "beauchef.cr460lab.com."
+}
+
+resource "google_dns_record_set" "jump" {
+    name = "jump.${google_dns_managed_zone.beauchef.dns_name}"
+    type = "A"
+    ttl = 300
+    managed_zone = "${google_dns_managed_zone.beauchef.name}"
+    rrdatas = ["${google_compute_instance.jumphost.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
+
+resource "google_dns_record_set" "vault" {
+    name = "vault.${google_dns_managed_zone.beauchef.dns_name}"
+    type = "A"
+    ttl = 300
+    managed_zone = "${google_dns_managed_zone.beauchef.name}"
+    rrdatas = ["${google_compute_instance.vaulthost.network_interface.0.access_config.0.assigned_nat_ip}"]
+}
